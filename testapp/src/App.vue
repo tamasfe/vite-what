628b6937d81e66bf503e0fdd14572ed39837e78d
Ferenc Tamás <tamasf97@outlook.com>
2021-07-04 20:25:25 +0200

    added working working component example

--- testapp/src/App.vue
+++ testapp/src/App.vue
@@ -1,11 +1,13 @@
 <template>
   <Test :obj="obj"></Test>
+  <Asd :obj="obj"></Asd>
 </template>
 
 <script setup lang="ts">
 import { defineProps, defineEmits, ref } from "vue";
 
 import { Test } from "testlib";
+import Asd from "./components/Asd.vue";
 
 const emit = defineEmits({});
 const props = defineProps<{}>();
---
+++ testapp/src/components/Asd.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="internal">
+    <template v-for="(val, name) in props.obj">{{ name }} {{ val }}</template>
+  </div>
+</template>
+
+<script setup lang="ts">
+const emit = defineEmits({});
+const props = defineProps<{
+  obj: Record<string, any>;
+}>();
+</script>
